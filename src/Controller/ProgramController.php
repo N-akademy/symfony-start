97f1e8e1b8e9a8071014645f3ecd9b51bfa4f0b6
N-akademy <56726632+N-akademy@users.noreply.github.com>
2022-01-18 16:09:15 +0100

    quetes 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -17,4 +17,12 @@ class ProgramController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
+    /**
+     * @Route("/program/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="program_show")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id,]);
+    }
 }
